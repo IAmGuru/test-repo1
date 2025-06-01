c00fb73fc328d2e7076819f2d710d6ffbb32b44e
IAmGuru <guruprasad019@yahoo.co.in>
2025-06-01 23:57:38 +0530

    app code updated

--- module1.py
+++ module1.py
@@ -1,1 +1,10 @@
-# test module
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hellow_world():
+    return "Hellow World !!"
+
+if __name__ == '__main__':
+    app.run(debug=False, host='localhost', port=5000)
