fd5b61d0a68360b1d36bdce7497a7da4eb77a7c4
IAmGuru <guruprasad019@yahoo.co.in>
2025-06-04 22:13:15 +0530

    app code changes committed

--- src/app.py
+++ src/app.py
@@ -1,10 +1,22 @@
+import logging
 from flask import Flask
 
 app = Flask(__name__)
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)  # Get a logger for your app
+
+app.logger.setLevel(logging.INFO)
 
 @app.route('/')
 def hellow_world():
     return "Hellow World !!"
 
 if __name__ == '__main__':
-    app.run(debug=False, host='localhost', port=5000)
+    try:
+        logger.info("flask app execution begins")
+        app.run(debug=False, host='localhost', port=5000)
+    except Exception as e:
+        logger.error("an error occurred: {e}")
+
